5f7585274494078933c05f4cfb96549f2bb8de2f
Alex Pan <alxtouch@gmail.com>
2023-03-18 15:34:17 +0200

    cut some bugs

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -38,5 +38,9 @@ avatar: PropTypes.string.isRequired,
 username: PropTypes.string.isRequired,
 tag: PropTypes.string.isRequired,
 location: PropTypes.string.isRequired,
-stats: PropTypes.object.isRequired,
+stats: PropTypes.shape({
+  followers: PropTypes.number.isRequired,
+  views: PropTypes.number.isRequired,
+  likes: PropTypes.number.isRequired,
+}),
 };
